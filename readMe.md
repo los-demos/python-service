eac9e802f24f2aec1d81152b489000e5e6202057
c2r79 <pharmsale7@gmail.com>
2025-05-03 14:47:06 -0400

    python service w/ k8 deploy

---
+++ readMe.md
@@ -0,0 +1,2 @@
+### Run the docker container:
+`docker run --rm -p 8080:5000 python-service:v1`
---
+++ src/app.py
@@ -0,0 +1,23 @@
+import os
+import datetime
+import socket
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+host = os.getenv("FLASK_RUN_HOST", "0.0.0.0")
+message = {"time": datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"), "hostname": socket.gethostname()}
+health_message = {"status": "up"}
+
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify(message)
+
+@app.route('/api/v1/health')
+
+def health():
+    return jsonify(health_message), 200
+
+if __name__ == '__main__':
+    app.run(host=host)
